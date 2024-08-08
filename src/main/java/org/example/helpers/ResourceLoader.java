132061ac3d3841ef0fabec5882baf4ca1c05d2cb
rodikno <rodikno@gmail.com>
2024-08-09 00:38:41 +0300

    Updated ResourceLoader to match GH actions server folder structure

--- src/main/java/org/example/helpers/ResourceLoader.java
+++ src/main/java/org/example/helpers/ResourceLoader.java
@@ -1,5 +1,7 @@
 package org.example.helpers;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -10,11 +12,16 @@ public class ResourceLoader {
     public static String getResourcePath(String filename) {
         ClassLoader classLoader = ResourceLoader.class.getClassLoader();
         URL resourceUrl = Objects.requireNonNull(classLoader.getResource(filename), "File not found: " + filename);
-        String path = resourceUrl.getPath();
-        if(path.startsWith("/")) {
-            path = path.substring(1);
+
+        // The URI is needed to deal with spaces and other special chars in paths
+        URI resourceUri;
+        try {
+            resourceUri = resourceUrl.toURI();
+        } catch (URISyntaxException e) {
+            throw new RuntimeException("Could not convert URL to URI: " + resourceUrl, e);
         }
-        Path p = Paths.get(path);
-        return p.toString();
+
+        Path path = Paths.get(resourceUri);
+        return path.toString();
     }
 }
